0e4863d232d330d471cfd44b46c8facb45f05148
babyshibainuonsol <babyshibainuonsol@gmail.com>
2026-04-13 04:37:49 -0400

    Update swap.js

--- api/swap.js
+++ api/swap.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
 
     res.status(200).json(data);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Server error" });
+    console.error("API ERROR:", err);
+    res.status(500).json({ error: err.message });
   }
 }
